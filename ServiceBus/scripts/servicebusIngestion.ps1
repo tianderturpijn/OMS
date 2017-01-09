b2750675590b0d9b797b0c8d04a3f5699603edd1
Tiander Turpijn <tiandert@microsoft.com>
2017-01-09 14:13:24 +0100

    Updated serviceBusIngestion runbook

--- ServiceBus/scripts/servicebusIngestion.ps1
+++ ServiceBus/scripts/servicebusIngestion.ps1
@@ -1,4 +1,4 @@
-﻿ # Suspend the runbook if any errors, not just exceptions, are encountered
+﻿# Suspend the runbook if any errors, not just exceptions, are encountered
 $ErrorActionPreference = "Stop"
 
 # ASM authentication
@@ -54,9 +54,6 @@ $Conn = Get-AutomationConnection -Name AzureRunAsConnection
 "Selecting Azure subscription..."
 $SelectedAzureSub = Select-AzureRmSubscription -SubscriptionId $Conn.SubscriptionID -TenantId $Conn.tenantid 
 
-#Get PaaS resource, Metrics Data, and Post to Ingestion API
-#Logtype will be the name of the custom log in Log Analytics 
-#Example: sqlazure will be sqlazure_CL once ingested 
 # Define Log Type 
 $logType  = "servicebus"
 "Logtype Name for ServiceBus(es) is '$logType'"
@@ -67,18 +64,25 @@ $logType  = "servicebus"
 	{
 		"Found $($sbList.Count) service bus namespace(s)."
 		
-		# Get resource usage metrics for a database in an elastic database for the specified time interval.
-		# This example will run every 10 minutes on a schedule and gather two data points for 15 metrics leveraging the ARM API 
 		foreach ($sb in $sbList)
 		{
 		    # Format metrics into a table.
-		    $table = @()
+		    $table1 = @()
+            $table2 = @()
 		    
 			"Processing service bus `"$($sb.Name)`"..."
 			    
 		    $sbAuth = Get-AzureSBAuthorizationRule -Namespace $sb.Name
 		    $nsManager = [Microsoft.ServiceBus.NamespaceManager]::CreateFromConnectionString($sbAuth.ConnectionString);
+
+#call Function queues here
+#GetAndPublishQueues
+
+
+            "Attempting to get queues...."
 		    $queueList = $nsManager.GetQueues()
+
+#region ForEach sqQueue
 		    foreach ($sbQueue in $queueList)
 		    {
 				#$sbQueue
@@ -93,6 +97,7 @@ $logType  = "servicebus"
 				{
 					"Unable to get queue for '$($sbQueue)': $_"
 				}
+
 				
 				if ($Queue -ne $null)
 				{
@@ -105,7 +110,49 @@ $logType  = "servicebus"
                         NamespaceType = $sb.Namespacetype.ToString();
                         ConnectionString = $sb.connectionString;
 			
-		            		
+			            <#
+			            ## Standard SB Message Properties
+			            
+			            LockDuration = $Queue.LockDuration;
+			            MaxSizeInMegabytes = $Queue.MaxSizeInMegabytes;
+			            RequiresDuplicateDetection = $Queue.RequiresDuplicateDetection;
+			            RequiresSession = $Queue.RequiresSession;
+			            DefaultMessageTimeToLive = $Queue.DefaultMessageTimeToLive;
+			            AutoDeleteOnIdle = $Queue.AutoDeleteOnIdle;
+			            EnableDeadLetteringOnMessageExpiration = $Queue.EnableDeadLetteringOnMessageExpiration;
+			            DuplicateDetectionHistoryTimeWindow = $Queue.DuplicateDetectionHistoryTimeWindow;
+			            Path = $Queue.Path;
+			            MaxDeliveryCount = $Queue.MaxDeliveryCount;
+			            EnableBatchedOperations = $Queue.EnableBatchedOperations;
+			            SizeInBytes = $Queue.SizeInBytes;
+			            MessageCount = $Queue.MessageCount;
+			
+			            #MessageCountDetails = $Queue.MessageCountDetails;
+			            ActiveMessageCount = $Queue.MessageCountDetails.ActiveMessageCount;
+			            DeadLetterMessageCount = $Queue.MessageCountDetails.DeadLetterMessageCount;
+			            ScheduledMessageCount = $Queue.MessageCountDetails.ScheduledMessageCount;
+			            TransferMessageCount = $Queue.MessageCountDetails.TransferMessageCount;
+			            TransferDeadLetterMessageCount = $Queue.MessageCountDetails.TransferDeadLetterMessageCount;
+			
+			            Authorization = $Queue.Authorization;
+			            IsAnonymousAccessible = $Queue.IsAnonymousAccessible;
+			            SupportOrdering = $Queue.SupportOrdering;
+			            Status = $Queue.Status;
+			            AvailabilityStatus = $Queue.AvailabilityStatus;
+			            ForwardTo = $Queue.ForwardTo;
+			            ForwardDeadLetteredMessagesTo = $Queue.ForwardDeadLetteredMessagesTo;
+			            CreatedAt = $Queue.CreatedAt;
+			            UpdatedAt = $Queue.UpdatedAt;
+			            AccessedAt = $Queue.AccessedAt;
+			            EnablePartitioning = $Queue.EnablePartitioning;
+			            UserMetadata = $Queue.UserMetadata;
+			            EnableExpress = $Queue.EnableExpress;
+			            IsReadOnly = $Queue.IsReadOnly;
+			            ExtensionData = $Queue.ExtensionData;
+			            
+			            ##
+			            #>
+			
 			            <# SB Message properties we care about #>
 			            Path = $Queue.Path;
 			            MaxDeliveryCount = $Queue.MaxDeliveryCount;
@@ -127,16 +174,63 @@ $logType  = "servicebus"
 			
 					$sx
 					
-			        $table = $table += $sx
+			        $table1 = $table1 += $sx
 			        
 			        # Convert table to a JSON document for ingestion 
 			        $jsonTable = ConvertTo-Json -InputObject $table
 				}
 			    #Post the data to the endpoint - looking for an "accepted" response code 
-		    	Send-OMSAPIIngestionData -customerId $customerId -sharedKey $sharedKey -body $jsonTable -logType $logType -TimeStampField $Timestampfield
+		    	#Send-OMSAPIIngestionData -customerId $customerId -sharedKey $sharedKey -body $jsonTable -logType $logType -TimeStampField $Timestampfield
 		    	# Uncomment below to troubleshoot
-		    	#$jsonTable
+		    	$jsonTable
 			}
+#endregion
+
+
+
+#call Function topics here
+#GetAndPublishTopics
+
+
+
+<# error handling topics       
+                "Attempting to get topics..."
+            	$topicList = $null
+				try
+				{
+		        	$topicList = $nsManager.GetTopics();
+				}
+				catch [exception]
+				{
+					"Unable to get topics, no topics found or something went wrong"
+				}
+#>
+#region ForEach Topics
+    		foreach ($topic in $topicList)
+		    {
+				
+				if ($topicList -ne $null)
+				{
+			        $sx2 = New-Object PSObject -Property @{
+						TimeStamp = $([DateTime]::Now.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"));
+                        TopicName = $topic.path;			            
+
+			        }
+			
+					$sx2
+					
+			        $table2 = $table2 += $sx2
+			        
+			        # Convert table to a JSON document for ingestion 
+			        $jsonTable = ConvertTo-Json -InputObject $table2
+				}
+			    #Post the data to the endpoint - looking for an "accepted" response code 
+		    	#Send-OMSAPIIngestionData -customerId $customerId -sharedKey $sharedKey -body $jsonTable -logType $logType -TimeStampField $Timestampfield
+		    	# Uncomment below to troubleshoot
+		    	$jsonTable
+			}
+#endregion
+
 		}
 	} else
 	{
@@ -144,5 +238,42 @@ $logType  = "servicebus"
 	}
 
 
+　
+<#
+foreach ($sub in $subscriptionList)
+{
+	"Processing subscription $($sub.SubscriptionId) `"$($sub.SubscriptionName)`"..."
+		
+	Publish-SBMetrics -azureSubscriptionID $sub.SubscriptionId	
+			
+}
+
+"Done processing all subscriptions."
+ 
+#>
+
+<#
+                        #RequiresDuplicateDetection = $topic.RequiresDuplicateDetection
+
+                        #ConnectionString = $sb.connectionString;
+                        #DefaultMessageTimeToLive = $topic.DefaultMessageTimeToLive
+                        #AutoDeleteOnIdle = $topic.AutoDeleteOnIdle
+                        #MaxSizeInMegabytes = $topic.MaxSizeInMegabytes
+                        #DuplicateDetectionHistoryTimeWindow = $topic.DuplicateDetectionHistoryTimeWindow
+                        #SizeInBytes = $topic.SizeInBytes
+                        #EnableBatchedOperations = $topic.EnableBatchedOperations
+                        #SupportOrdering = $topic.SupportOrdering
+                        #EnableFilteringMessagesBeforePublishing = $topic.EnableFilteringMessagesBeforePublishing
+                        #IsAnonymousAccessible = $topic.IsAnonymousAccessible
+                        #Status = $topic.Status
+                        #AvailabilityStatus = $topic.AvailabilityStatus
+                        #CreatedAt = $topic.CreatedAt
+                        #UpdatedAt = $topic.UpdatedAt
+                        #AccessedAt = $topic.AccessedAt
+                        #SubscriptionCount = $topic.SubscriptionCount
+                        #EnablePartitioning = $topic.EnablePartitioning
+                        #EnableExpress = $topic.EnableExpress
+                        #IsReadOnly = $topic.IsReadOnly
+#>
  
 
